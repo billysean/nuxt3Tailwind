974227f85446873b9bab725c4a743da0796447e9
Billy <billysean.tj@gmail.com>
2022-01-20 15:36:54 +0700

    add tailwind dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./assets/**/*.{css}",
+    "./Layouts/*.vue",
+    "./components/*.{vue,js}",
+    "./components/**/*.{vue,js}",
+    "./pages/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./*.{vue,js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
