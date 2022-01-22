1bc860593ea17dece9f231b5548419d384b52937
Billy <billysean.tj@gmail.com>
2022-01-22 22:12:55 +0800

    config : setup supabase connection on initial layer

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup>
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseURL = process.env.SUPABASE_URL
+const supabaseAPIKey = process.env.SUPABASE_PUBLIC_KEY
+
+const supabase = createClient(supabaseURL,supabaseAPIKey)
+console.log(supabase)
+</script>
+
+
 <template>
   <div>
     <NuxtPage />
--- server/api/supabase.js
+++ server/api/supabase.js
@@ -1,3 +1,3 @@
-export default (req,res){
-    return "a";
-}
+// export default (req,res){
+//     return "a";
+// }
