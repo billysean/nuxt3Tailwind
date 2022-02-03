d3d481d9e52e4894ec67ed85fd9aa0f4f53d8ecf
Billy <billysean.tj@gmail.com>
2022-02-03 12:53:09 +0700

    feat : blogs index view

--- pages/Blogs/index.vue
+++ pages/Blogs/index.vue
@@ -1,3 +1,40 @@
+<script setup>
+import blogsTable from "~/components/blogsTable";
+</script>
+
 <template>
-    
+  <button
+    type="button"
+    class="
+      inline-flex
+      items-center
+      px-6
+      py-3
+      border border-transparent
+      text-base
+      font-medium
+      rounded-full
+      shadow-sm
+      text-white
+      bg-indigo-600
+      hover:bg-indigo-700
+      focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+    "
+  >  
+  Add New Blog
+    <PlusSmIconOutline class="h-6 w-6" aria-hidden="true" />
+  </button>
+  <blogsTable class="mt-3" />
 </template>
+
+<script>
+import { PlusSmIcon as PlusSmIconSolid } from '@heroicons/vue/solid'
+import { PlusSmIcon as PlusSmIconOutline } from '@heroicons/vue/outline'
+
+export default {
+  components: {
+    PlusSmIconOutline,
+    PlusSmIconSolid,
+  },
+}
+</script>
